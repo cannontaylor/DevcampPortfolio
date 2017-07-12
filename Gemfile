17d9bb72c89425b5025afdeab6f27007185fe340
Jordan Hudgens <jordan@devcamp.com>
2017-07-12 17:54:04 +0000

    Integrated new gem into rails app for copyright

--- Gemfile
+++ Gemfile
@@ -56,3 +56,5 @@ gem 'friendly_id', '~> 5.1.0'
 gem 'devise', '~> 4.3'
 
 gem 'bootstrap', git: 'https://github.com/twbs/bootstrap-rubygem'
+
+gem 'taylor_view_tool', git: 'https://github.com/cannontaylor/taylor_view_tool'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+  
+  def copyright_generator
+    TaylorViewTool::Renderer.copyright 'Cannon Taylor', 'All rights reserved'
+  end
 end
